78fbdc7b10b75ca9d9a50e232d232ecbb69014ea
Raed <raed.zayoud.23@gmail.com>
2025-01-26 12:13:38 +0100

    add font family to main

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:book/constant.dart';
 import 'package:book/features/splash/presentation/view/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const EasyBookApp());
@@ -13,7 +14,9 @@ class EasyBookApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: KPrimaryColor),
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: KPrimaryColor,
+      textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)
+      ),
       home: SplashView(),
     );
   }
