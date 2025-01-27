b548e68b793012f6484b3f1fbbbca1c37c11dc84
Raed <raed.zayoud.23@gmail.com>
2025-01-27 11:04:24 +0100

    Add Style part 2

--- lib/core/utlis/styles.dart
+++ lib/core/utlis/styles.dart
@@ -1,8 +1,19 @@
+import 'package:book/constant.dart';
 import 'package:flutter/material.dart';
 
 abstract class AppStyles {
 
   static const textStyle18 =
       TextStyle(fontSize: 18, fontWeight: FontWeight.w600);
+  static const textStyle20 =
+      TextStyle(fontSize: 20, fontWeight: FontWeight.normal,fontFamily: KGTSectraFine);
+  static const textStyle30 =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.normal);
+  
+  static const textStyle14 =
+      TextStyle(fontSize: 14, fontWeight: FontWeight.normal);
+  
+  static const textStyle16 =
+      TextStyle(fontSize: 16, fontWeight: FontWeight.w500);
 
 }
