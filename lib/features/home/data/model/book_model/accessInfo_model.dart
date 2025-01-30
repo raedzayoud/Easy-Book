ec441182250083942331432e317ce8eada502ea2
Raed <raed.zayoud.23@gmail.com>
2025-01-30 23:17:00 +0100

    Create books model

---
+++ lib/features/home/data/model/book_model/accessInfo_model.dart
@@ -0,0 +1,56 @@
+class AccessInfo {
+  String? country;
+  String? viewability;
+  bool? embeddable;
+  bool? publicDomain;
+  String? textToSpeechPermission;
+  Epub? epub;
+  Pdf? pdf;
+  String? webReaderLink;
+  String? accessViewStatus;
+  bool? quoteSharingAllowed;
+
+  AccessInfo(
+      {this.country,
+      this.viewability,
+      this.embeddable,
+      this.publicDomain,
+      this.textToSpeechPermission,
+      this.epub,
+      this.pdf,
+      this.webReaderLink,
+      this.accessViewStatus,
+      this.quoteSharingAllowed});
+
+  AccessInfo.fromJson(Map<String, dynamic> json) {
+    country = json['country'];
+    viewability = json['viewability'];
+    embeddable = json['embeddable'];
+    publicDomain = json['publicDomain'];
+    textToSpeechPermission = json['textToSpeechPermission'];
+    epub = json['epub'] != null ? new Epub.fromJson(json['epub']) : null;
+    pdf = json['pdf'] != null ? new Pdf.fromJson(json['pdf']) : null;
+    webReaderLink = json['webReaderLink'];
+    accessViewStatus = json['accessViewStatus'];
+    quoteSharingAllowed = json['quoteSharingAllowed'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['country'] = this.country;
+    data['viewability'] = this.viewability;
+    data['embeddable'] = this.embeddable;
+    data['publicDomain'] = this.publicDomain;
+    data['textToSpeechPermission'] = this.textToSpeechPermission;
+    if (this.epub != null) {
+      data['epub'] = this.epub!.toJson();
+    }
+    if (this.pdf != null) {
+      data['pdf'] = this.pdf!.toJson();
+    }
+    data['webReaderLink'] = this.webReaderLink;
+    data['accessViewStatus'] = this.accessViewStatus;
+    data['quoteSharingAllowed'] = this.quoteSharingAllowed;
+    return data;
+  }
+}
