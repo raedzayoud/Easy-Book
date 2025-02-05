61e2fe459d3c29e5366508e68e645ae018b74242
Raed <raed.zayoud.23@gmail.com>
2025-02-05 09:57:46 +0100

    Book Details Done

--- lib/features/home/presentation/view/widget/book_button.dart
+++ lib/features/home/presentation/view/widget/book_button.dart
@@ -1,3 +1,4 @@
+import 'package:book/core/utlis/function/launchcustomurl.dart';
 import 'package:book/core/widgets/custom_button.dart';
 import 'package:book/features/home/data/model/book_model/book_model.dart';
 import 'package:flutter/material.dart';
@@ -24,19 +25,23 @@ class BookButton extends StatelessWidget {
         Expanded(
             child: CustomButton(
           onPressed: () async {
-            Uri uri = Uri.parse(bookModel.volumeInfo!.previewLink!);
-            if (await canLaunchUrl(uri)) {
-              await launchUrl(uri);
-            }
+            launchCustomUrl(context, bookModel.volumeInfo?.previewLink);
           },
           backgroundColor: Color(0xFFEF8262),
           borderRadius: BorderRadius.only(
               bottomRight: Radius.circular(12), topRight: Radius.circular(12)),
           textColor: Colors.white,
-          text: "Preview ",
+          text: getText(bookModel),
           fontSize: 15,
         )),
       ],
     );
+  }
+
+  String getText(BookModel bookModel) {
+    if (bookModel.volumeInfo?.previewLink == null) {
+      return "Not Avaliable";
+    }
+    return "Preview";
   }
 }
